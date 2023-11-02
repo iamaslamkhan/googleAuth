a34abd2566fbf641da0d8f8ddb425b7e98db67bc
Aslam Asgar Khan <106332798+iamaslamkhan@users.noreply.github.com>
2023-11-02 09:37:34 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+        apiKey: "AIzaSyA6WbWNfuKSW6EtFaqIdqpM40javKcbA88",
+        authDomain: "central-space-c4466.firebaseapp.com",
+        databaseURL: "https://central-space-c4466-default-rtdb.firebaseio.com",
+        projectId: "central-space-c4466",
+        storageBucket: "central-space-c4466.appspot.com",
+        messagingSenderId: "455334809949",
+        appId: "1:455334809949:web:4b726648c17bd5ce8418b4",
+        measurementId: "G-42BJPH8Q0C"
+};
+export {firebaseConfig};
